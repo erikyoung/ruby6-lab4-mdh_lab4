cee5c2683742218afcbdfd8b8c148097522fa9fc
erikyoung <erik.young@rmit.edu.vn>
2017-07-05 20:06:45 +0700

    added room god dammit

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,7 @@ class Room < ApplicationRecord
 
 has_many :rooms
  validates :name, presence: :true
- validates :email, presence: :true, uniqueness: :true
+ 
 
   has_many :messages
   validates :name, uniqueness: true, presence: true

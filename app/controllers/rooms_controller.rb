ed53c905c805f25e1b5a8c2bcd657fee09f3ac00
Hai Minh Do <dhminh1024@gmail.com>
2017-07-05 19:11:26 +0700

    add messages

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,22 @@
+class RoomsController < ApplicationController
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      flash[:success] = "Create Room #{@room.name} sucessfully."
+      redirect_back fallback_location: root_path
+    else
+      flash[:error] = "Error: #{@room.errors.full_messages.to_sentence}"
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  def show
+    @room = Room.find_by(id: params[:id])
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101095731) do
+ActiveRecord::Schema.define(version: 20170705120734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
-    t.text     "content"
     t.string   "username"
+    t.text     "content"
+    t.integer  "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id", using: :btree
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 20161101095731) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "rooms"
 end
